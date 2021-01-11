287c45421b690ce920e7af16ea840d11f22b6aa0
oleglego94 <olegik94b@gmail.com>
2021-01-11 20:07:22 +0200

    update tests

--- tests/test_converting.py
+++ tests/test_converting.py
@@ -1,19 +1,23 @@
 import pytest
-from page_loader import convert
+from page_loader.downloading import convert_url
 
 
 url = "https://ru.hexlet.io/courses"
-result = "ru-hexlet-io-courses.html"
+result = "ru-hexlet-io-courses"
 
 url1 = "https://ru.hexlet.io/"
-result1 = "ru-hexlet-io.html"
+result1 = "ru-hexlet-io"
 
 url2 = "https://ru.hexlet.io/projects/51/members/12473?step_id=217"
-result2 = "ru-hexlet-io-projects-51-members-12473-step_id-217.html"
+result2 = "ru-hexlet-io-projects-51-members-12473-step_id-217"
 
 
 @pytest.mark.parametrize(
-    "url, result", [(url, result), (url1, result1), (url2, result2)]
+    "url, result", [
+        (url, result),
+        (url1, result1),
+        (url2, result2),
+    ]
 )
 def test_convert(url, result):
-    assert convert(url) == result
+    assert convert_url(url) == result
--- tests/test_downloading.py
+++ tests/test_downloading.py
@@ -1,11 +1,16 @@
 import os
+import requests_mock
 from tempfile import TemporaryDirectory
-from page_loader import download
+from page_loader import download_page
 
 
-def test_download():
+def test_download_page():
     with TemporaryDirectory() as tempdir:
-        url = "https://ru.hexlet.io/courses"
-        file_path = os.path.join(tempdir, "ru-hexlet-io-courses.html")
-        assert download(url, tempdir) == file_path
-        assert os.path.isfile(file_path) is True
+        with requests_mock.Mocker() as mock:
+            with open("tests/fixtures/test_page.html", "r") as t:
+                response = t.read()
+            mock_url = mock.get("http://test.com", text=response)
+            page_path = download_page("http://test.com", tempdir)
+            assert page_path.endswith(".html") is True
+            with open(page_path, "r") as page:
+                assert page.readline() == "<!DOCTYPE html>\n"
