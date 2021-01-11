e14b7e859e61916dc8539d9af0ba10d0405ee45c
oleglego94 <olegik94b@gmail.com>
2021-01-11 20:09:50 +0200

    correct mistakes from linter

--- tests/test_downloading.py
+++ tests/test_downloading.py
@@ -1,4 +1,3 @@
-import os
 import requests_mock
 from tempfile import TemporaryDirectory
 from page_loader import download_page
@@ -9,7 +8,7 @@ def test_download_page():
         with requests_mock.Mocker() as mock:
             with open("tests/fixtures/test_page.html", "r") as t:
                 response = t.read()
-            mock_url = mock.get("http://test.com", text=response)
+            mock.get("http://test.com", text=response)
             page_path = download_page("http://test.com", tempdir)
             assert page_path.endswith(".html") is True
             with open(page_path, "r") as page:
