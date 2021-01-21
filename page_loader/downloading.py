c46b232f93abfb9d068f99fcb6474ecf628ce638
oleglego94 <olegik94b@gmail.com>
2021-01-21 23:21:43 +0200

    implement function download_resources

--- page_loader/downloading.py
+++ page_loader/downloading.py
@@ -1,22 +1,28 @@
-import re
 import os
-import requests
 from page_loader.cli import DEFAULT_DIR
+from page_loader.naming import make_name
+from page_loader.resources import get_local_resources
+from page_loader.saving import save
 
 
-def download_page(url, directory=DEFAULT_DIR):
-    response = requests.get(url)
-    name = convert_url(url)
+def download(url, directory=DEFAULT_DIR):
+    page = download_page(url, directory)
+    download_resources(page, url, directory)
+    return page
 
-    html_path = os.path.join(directory, name + ".html")
-    with open(html_path, "wb") as f:
-        f.write(response.content)
 
+def download_page(source, storage):
+    html_name = make_name(source, ".html")
+    html_path = os.path.join(storage, html_name)
+
+    save(source, html_path)
     return os.path.abspath(html_path)
 
 
-def convert_url(url):
-    if url.endswith("/"):
-        url = url[:-1]
-    match = re.split(".*://", url)
-    return re.sub(r"\W", "-", match[1])
+def download_resources(html_path, source, storage):
+    resources_dir = make_name(source, "_files")
+    resources_path = os.path.join(storage, resources_dir)
+    os.mkdir(resources_path)
+
+    get_local_resources(html_path, resources_path, source)
+    return os.path.abspath(resources_path)
