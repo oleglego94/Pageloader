61c3ef09534f011d8b344992abb3944cc4be1d86
oleglego94 <olegik94b@gmail.com>
2021-01-21 23:16:12 +0200

    add fixtures and tests

--- tests/test_downloading.py
+++ tests/test_downloading.py
@@ -1,15 +1,45 @@
+import os
 import requests_mock
 from tempfile import TemporaryDirectory
-from page_loader import download_page
+from page_loader import io
+from page_loader.downloading import download_page, download_resources
+
+URL = "http://test.com"
+RESOURCES = {
+    URL: "tests/fixtures/test_page.html",
+    "http://test.com/assets/application.css": "tests/fixtures/files/test.css",
+    "http://test.com/courses": "tests/fixtures/files/test.html",
+    "http://test.com/assets/professions/nodejs.png": "tests/fixtures/files/test.png",  # noqa: E501
+    "http://test.com/packs/js/runtime.js": "tests/fixtures/files/test.js",
+}
 
 
-def test_download_page():
+def test_download_page_():
     with TemporaryDirectory() as tempdir:
         with requests_mock.Mocker() as mock:
-            with open("tests/fixtures/test_page.html", "r") as t:
-                response = t.read()
-            mock.get("http://test.com", text=response)
-            page_path = download_page("http://test.com", tempdir)
-            assert page_path.endswith(".html") is True
-            with open(page_path, "r") as page:
-                assert page.readline() == "<!DOCTYPE html>\n"
+            mock.get(URL, text="test")
+            page_path = download_page(URL, tempdir)
+        assert os.path.exists(page_path) is True
+        assert page_path.endswith(".html") is True
+
+
+def test_download_resources():
+    with TemporaryDirectory() as tempdir:
+        with requests_mock.Mocker() as mock:
+
+            for url, content_path in RESOURCES.items():
+                if url.endswith((".png", ".jpg")):
+                    response = io.read_file(content_path, "rb")
+                response = io.read_file(content_path)
+                mock.get(url, text=response)
+
+            page_path = download_page(URL, tempdir)
+            resources_path = download_resources(page_path, URL, tempdir)
+
+        assert os.path.exists(resources_path) is True
+        assert resources_path.endswith("_files") is True
+
+        with open(page_path, "r") as modified_html, open(
+            "tests/fixtures/modified_test_page.html", "r"
+        ) as sample:
+            assert modified_html.read() == sample.read()
---
+++ tests/test_resources.py
@@ -0,0 +1,48 @@
+import pytest
+from page_loader import resources
+
+URL = "http://test.com"
+DOMAIN = "http://test.com"
+
+
+def test_get_domain():
+    url = "http://test.com/projects/51/members/12473?step_id=219"
+    assert resources.get_domain(url) == URL
+
+
+@pytest.mark.parametrize(
+    "url, domain, result",
+    [
+        ("https://cdn2.hexlet.io/assets/menu.css", DOMAIN, False),
+        ("https://test.com/v3/", DOMAIN, False),
+        ("http://test.com/packs/js/runtime.js", DOMAIN, True),
+        ("http://test.com/professions/nodejs", DOMAIN, True),
+    ],
+)
+def test_is_local(url, domain, result):
+    assert resources.is_local(url, domain) == result
+
+
+@pytest.mark.parametrize(
+    "url, directory, path",
+    [
+        (
+            "https://test.com/assets/menu.css",
+            "test-com_files",
+            "test-com_files/test-com-assets-menu.css",
+        ),
+        (
+            "https://test.com/packs/js/runtime.js",
+            "test-com_files",
+            "test-com_files/test-com-packs-js-runtime.js",
+        ),
+        (
+            "http://test.com/assets/professions/nodejs.png",
+            "test-com_files",
+            "test-com_files/test-com-assets-professions-nodejs.png",
+        ),
+    ],
+)
+def test_make_file_path(url, directory, path):
+    _, rel_path = resources.make_file_path(url, directory)
+    assert rel_path == path
