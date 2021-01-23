8240340baedfc8dee6a0f0a02dcd53c20540fdf5
oleglego94 <olegik94b@gmail.com>
2021-01-23 21:43:34 +0200

    change level

--- page_loader/logging.py
+++ page_loader/logging.py
@@ -4,7 +4,7 @@ import logging
 
 def setup_logging():
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.WARNING,
         stream=sys.stderr,
         format="[%(asctime)s]-[%(levelname)s]-%(filename)s:%(lineno)d => %(message)s",  # noqa: E501
         datefmt="%Y-%m-%d %H:%M:%S",
