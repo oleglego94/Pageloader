8d23a415ab904caa815044d207db5f1c6d5b2287
oleglego94 <olegik94b@gmail.com>
2021-01-23 17:17:03 +0200

    add logging

---
+++ page_loader/logging.py
@@ -0,0 +1,11 @@
+import sys
+import logging
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stderr,
+        format="[%(asctime)s]-[%(levelname)s]-%(filename)s:%(lineno)d => %(message)s",  # noqa: E501
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
