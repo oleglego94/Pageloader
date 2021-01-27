66932b014b4004287a6fa07215751d758f482e67
oleglego94 <olegik94b@gmail.com>
2021-01-28 00:09:03 +0200

    delete if else

--- page_loader/saving.py
+++ page_loader/saving.py
@@ -1,4 +1,5 @@
 import requests
+
 from page_loader import io
 from page_loader.logging import setup_logging
 
@@ -8,10 +9,8 @@ setup_logging()
 def save(source, file_path):
     try:
         response = requests.get(source)
-        code = response.status_code
-        if code == requests.codes.ok:
-            io.write_file(response.content, file_path)
-        else:
-            response.raise_for_status()
+        response.raise_for_status()
+        io.write_file(response.content, file_path)
+
     except requests.exceptions.ConnectionError as error:
         raise error
