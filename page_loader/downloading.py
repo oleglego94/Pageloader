0277adb1369a218de750a691bf34d07b4991211e
oleglego94 <olegik94b@gmail.com>
2021-02-08 01:37:36 +0200

    redesign architecture

--- page_loader/downloading.py
+++ page_loader/downloading.py
@@ -1,46 +1,66 @@
-import logging
+import logging as log
 import os
 
-from page_loader import io
-from page_loader.cli import DEFAULT_DIR
-from page_loader.io import create_directory
-from page_loader.logging import setup_logging
-from page_loader.naming import make_name
-from page_loader.resources import get_local_resources
-from page_loader.saving import save
+import requests
+from progress.bar import PixelBar
 
-setup_logging()
+from page_loader import dom, storage, url
+from page_loader.cli import DEFAULT_OUTPUT
 
 
-def download(url, directory=DEFAULT_DIR):
-    logging.info("Downloading html")
-    page = download_page(url, directory)
+def download(page_url, output=DEFAULT_OUTPUT):
+    log.info("Downloading page")
+    html = load(page_url)
+    log.debug(f"{page_url} downloaded")
 
-    logging.info("Downloading resources")
-    download_resources(page, url, directory)
-    return page
-
-
-def download_page(source, storage):
-    html_name = make_name(source, ".html")
-    html_path = os.path.join(storage, html_name)
+    html_path = os.path.join(output, url.to_file_name(page_url, ".html"))
     if os.path.exists(html_path):
-        raise OSError(f"File exists: '{html_path}'")
-    save(source, html_path)
-    logging.info(f"{source} was successfully downloaded into '{html_name}'")
-    return os.path.abspath(html_path)
+        raise FileExistsError(f"File exists: '{html_path}'")
+
+    dir_path = os.path.join(output, url.to_dir_name(page_url))
+    if os.path.exists(dir_path):
+        raise FileExistsError(f"Directory exists: '{dir_path}'")
+
+    html_handled, resources = dom.handle_html(html, page_url, dir_path)
+    log.info("Saving page")
+    storage.save(html_handled, os.path.abspath(html_path))
+    log.debug("Handled HTML saved")
+
+    if resources:
+        storage.create_directory(dir_path)
+        log.debug("Directory created")
+        log.info("Downloading resources")
+        download_resources(resources)
+    return html_path
 
 
-def download_resources(html_path, source, storage):
-    resources_dir = make_name(source, "_files")
-    resources_path = os.path.join(storage, resources_dir)
-    create_directory(resources_path)
-    logging.info(f"{resources_dir} was successfully created")
-    new_html = get_local_resources(html_path, resources_path, source)
-    rewrite_html(new_html, html_path)
-    return os.path.abspath(resources_path)
+def load(link):
+    try:
+        response = requests.get(link)
+        response.raise_for_status()
+        return response.text if response.encoding else response.content
+
+    except requests.exceptions.RequestException:
+        raise
 
 
-def rewrite_html(html, path):
-    io.write_file(html, path, "w")
-    logging.info(f"Links in {path} were successfully changed")
+def download_resources(resources: dict):
+    with PixelBar(
+        "\U0001F4E5 Downloading resources",
+        max=len(resources),
+    ) as bar:
+        for resource_url, resource_path in resources.items():
+            try:
+                content = load(resource_url)
+                log.debug(f"{resource_url} downloaded")
+            except requests.exceptions.RequestException:
+                log.warning(f"{resource_url} not downloaded")
+            else:
+                path = os.path.abspath(resource_path)
+                try:
+                    storage.save(content, path)
+                    log.debug(f"'{resource_path}' saved")
+                except OSError:
+                    log.warning(f"'{resource_path}' not saved")
+                finally:
+                    bar.next()
